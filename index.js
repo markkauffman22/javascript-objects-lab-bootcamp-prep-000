2a35024f38136763e92cdc8dac51c39cf3988077
Mark Kauffman <markkauffman22@gmail.com>
2019-09-20 04:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, { [key]: value })
+  return Object.assign({}, obj, { [key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, { [key]: value })
+  return Object.assign(object, { [key]: value });
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = object.assign({}, obj)
-  delete newObj.key
-  return newObj
+  var newObj = object.assign({}, obj);
+  delete newObj.key;
+  return newObj;
 }
