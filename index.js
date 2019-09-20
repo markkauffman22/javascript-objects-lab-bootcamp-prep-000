3568f9a83e2900e6e0ec9879181bdb36d242caaa
Mark Kauffman <markkauffman22@gmail.com>
2019-09-20 04:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(obj, { [key]: value })
+  return Object.assign(object, { [key]: value })
 }
