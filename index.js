6f7357ca2e024032cebc1f982734f39da218b523
Mark Kauffman <markkauffman22@gmail.com>
2019-09-20 04:59:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, { [key]: value });
 }
 
-// Note below .;; using bracket notation  newObj[key] not newObj.key to get value to undefined
+// Note below ... MUST USE bracket notation newObj[key] not newObj.key to get value to undefined
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj[key];
