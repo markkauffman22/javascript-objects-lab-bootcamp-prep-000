eab1c372cebdc4f7230d7c9ca11696625b7eaeb1
Mark Kauffman <markkauffman22@gmail.com>
2019-09-20 04:17:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = object.assign({}, obj);
+  var newObj = object.assign({}, object);
   delete newObj.key;
   return newObj;
 }
