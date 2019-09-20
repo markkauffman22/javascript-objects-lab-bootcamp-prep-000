fb07434837a341e33c51c815cf0a1234cf7d8a1e
Mark Kauffman <markkauffman22@gmail.com>
2019-09-20 05:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 // Note below ... MUST USE bracket notation newObj[key] not newObj.key as key is a passed in var.
-// 
+// When we use dot notation the key is always taken as the literal string provided.
+// We must use bracket notation if we want to access (or delete) values that belong to a variable key.
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj[key];
